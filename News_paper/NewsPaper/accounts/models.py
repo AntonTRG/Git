8849671f359934740d813fa421dc41c4b4805e43
AntonTRG <93707580+AntonTRG@users.noreply.github.com>
2022-01-11 14:10:40 +0500

    D2.9

---
+++ News_paper/NewsPaper/accounts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Author(models.Model):
+    pass
+
+
+class User(models.Model):
+    pass
+
+# Create your models here.
---
+++ pythonProject2/test order/mcdonaldsproject/mcapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class McappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mcapp'
